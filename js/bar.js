ff87a2b2f4d7158d4a68acdb34da3e876362a0a2
Brian Greig <brian@resurgencewebdesign.com>
2017-06-11 22:04:45 -0400

    initial commit

--- js/bar.js
+++ js/bar.js
@@ -4,140 +4,76 @@
 // Graph is an object that is accessible throughout the IIFE
 // that contains all of the metadata about the graph and the container.
 var barGraph = {
-    container: {
-        svg: null,
-        xOffset: 60,
-        yOffset: 0,
-        height: 300,
-        width: 650
-    },
-    rects: null,
-    xScale: null,
-    yScale: null,
-    xAxis: null,
-    yAxis: null,
-    metric: null,
-    x: function(d) {
-        return this.container.xOffset + this.xScale(d['age'])
-    },
-    y: function(d) {
-        return this.yScale(d[this.metric]);
-    },
-    height: function(d) {
-        return (this.yScale(0) - this.yScale(d[this.metric]));
-    },
+  container: {
+    svg: null,
+    height: 300,
+    width: 650
+  },
+  rects: null,
+  metric: null,
 
-    // tooltips will display the info about the currently highlignted item.
-    _toolTip: function(d) {
-        this.container.svg.append("text")
-            .attr("x", this.xScale(d['age']))
-            .attr("y", this.yScale(d[this.metric]))
-            .attr("class", "tt")
-            .text("Age: " + d.age + " Pop: " + d[this.metric]);
-    },
+  // _initPhase is going to set the metric, scales, domains, and axis
+  // it will bind the data to our svg element
+  _init: function(d, m) {
+    this.metric = m;
+    console.log(d);
+    this.rects = this.container.svg.selectAll("rect")
+        .data(d)
+    this.text = this.container.svg.selectAll("text")
+        .data(d)
+  },
 
-    //Remove tooltip will erase it from the screen on mouseout.
-    _removeToolTip: function(d) {
-        this.container.svg.selectAll(".tt").remove();
-    },
+  // _enterPhase will run once when our svg is first rendered
+  // we will set any static attributes here
+  _enter: function(d) {
+    let self = this;
+    self.rects.enter()
+        .append("rect")
+        .attr("width", function(d){ return d.value / 50000 })
+        .attr("y", function(d, i){ return 20 * i })
+        .attr("height", 18)
+        .attr("x", 100)
 
-    // _drawAxis draws a new x and y axis based off of the offsets
-    // in the this object
-    _drawAxis: function(d) {
-			self = this;
-        self.container.svg.selectAll("g").remove();
-        self.container.svg.selectAll("g").data(d).enter()
-            .append("g")
-            .attr("transform", "translate(" + self.container.xOffset + "," + self.container.yOffset + ")")
-            .call(self.yAxis);
+    self.text.enter()
+        .append("text")
+        .attr("x", 0)
+        .attr("y", function(d, i){ return 20 * i + 16 })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "16px")
+        .text(function(d){ return d.data.name })
+  },
 
-        self.container.svg.append("g")
-            .attr("transform", "translate(" + self.container.xOffset + ",275)")
-            .call(self.xAxis);
-    },
+  // _updatePhase will run every time the graph is redrawn
+  // we will only update dynamic elements here
+  _update: function(d) {
+    let self = this;
+  },
 
-    // _initPhase is going to set the metric, scales, domains, and axis
-    // it will bind the data to our svg element
-    _init: function(d, m) {
-        this.metric = m;
-        this.xScale = d3.scaleLinear()
-            .domain([
-                d3.min(d, function(d) {
-                    return d.age;
-                }),
-                d3.max(d, function(d) {
-                    return d.age;
-                })
-            ])
-            .range([0, 540]);
+  // _exitPhase is for cleanup
+  _exit: function() {
+    this.rects.exit().remove();
+  },
 
-        this.yScale = d3.scaleLinear()
-            .domain([500000, 5000000])
-            .range([250, 25]);
-        this.yAxis = d3.axisLeft().scale(this.yScale);
-        this.xAxis = d3.axisBottom().scale(this.xScale);
+  // _render function should be called every time you want to redraw the graph
+  // takes as parameters the data and the metric name
+  render: function(d, m) {
 
-        this.rects = this.container.svg.selectAll("rect")
-            .data(d)
-    },
+    var heir = d3.hierarchy(d, function(d){
+      return d.regions;
+    });
 
-    // _enterPhase will run once when our svg is first rendered
-    // we will set any static attributes here
-    _enter: function(d) {
-        let self = this;
-        let w = (480 / d.length);
-        self.rects.enter()
-            .append("rect")
-            .attr("class", "bar")
-            .attr("width", w)
-            .attr("y", function(d) {
-                return self.y(d);
-            })
-            .attr("height", function(d) {
-                return self.height(d);
-            })
-            .attr("x", function(d) {
-                return self.x(d);
-            })
-            .on("mouseover", function(d) {
-                self._toolTip(d);
-            })
-            .on("mouseout", function(d) {
-                self._removeToolTip(d);
-            });
-        self._drawAxis(d)
-    },
+    var root = heir.sum(function(d){
+      return d.population;
+    });
 
-    // _updatePhase will run every time the graph is redrawn
-    // we will only update dynamic elements here
-    _update: function(d) {
-        let self = this;
-        let w = Math.floor(540 / d.length);
-        self.rects.transition().
-        duration(750)
-            .attr("width", w)
-            .attr("y", function(d) {
-                return self.y(d);
-            })
-            .attr("height", function(d) {
-                return self.height(d);
-            })
-            .attr("x", function(d) {
-                return self.x(d);
-            });
-    },
-
-    // _exitPhase is for cleanup
-    _exit: function() {
-        this.rects.exit().remove();
-    },
-
-    // _render function should be called every time you want to redraw the graph
-    // takes as parameters the data and the metric name
-    render: function(d, m) {
-        this._init(d, m);
-        this._enter(d);
-        this._update(d);
-        this._exit();
-    }
+    // h.children.forEach(function(d){
+    //   root = d.sum(function(d){return d.population});
+    //   console.log("- " + root.data.name + ":" + root.value);
+    // })
+    var data = root.children[0].children[1].children;
+    this._init(data, m);
+    this._enter(data);
+    this._update(data);
+    this._exit();
+  }
 };
--- js/main.js
+++ js/main.js
@@ -1,66 +1,11 @@
 // initiate, render, and handle input for our events
 
 d3.queue()
-  .defer(d3.json, "data/age.json")
-  .defer(d3.json, "data/generation.json")
-  .await(function(error, d, g) {
+  .defer(d3.json, "data/state_pop.json")
+  .await(function(error, d) {
     // create our svg element on the page
     barGraph.container.svg = d3.select("#graph").append('svg')
       .attr("height", barGraph.container.height)
       .attr("width", barGraph.container.width);
-
-    pieChart.container.svg = d3.select("#chart").append('svg')
-      .attr("height", pieChart.container.height)
-      .attr("width", pieChart.container.width)
-
-    var dg = d.map(function(obj) {
-      var title = "";
-      g.forEach(function(e) {
-        if (obj.age >= e.min && obj.age <= e.max) {
-          title = e.title
-        }
-      })
-      return {
-        "generation": title,
-        "total": obj.total,
-        "males": obj.males,
-        "females": obj.females
-      };
-    });
-    var grp = d3.nest()
-      .key(function(d) {
-        return d.generation;
-      })
-      .rollup(function(v) {
-        return d3.sum(v, function(d) {
-          return d.total;
-        })
-      })
-      .entries(dg);
-
     barGraph.render(d, "total");
-    pieChart.render(grp, "value");
-
-    //add our event listeners for the buttons
-    document.getElementById("total").addEventListener('click', function() {
-      barGraph.render(d, "total")
-    });
-    document.getElementById("male").addEventListener('click', function() {
-      barGraph.render(d, "males")
-    });
-    document.getElementById("female").addEventListener('click', function() {
-      barGraph.render(d, "females");
-    })
-    document.getElementById("over50").addEventListener('click', function() {
-      let td = d.filter(function(a) {
-        return a.age >= 50
-      });
-      barGraph.render(td, "total");
-    });
-    document.getElementById("under50").addEventListener('click', function() {
-      let td = d.filter(function(a) {
-        return a.age < 50
-      });
-      barGraph.render(td, "total");
-    });
   });
